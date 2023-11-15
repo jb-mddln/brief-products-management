6543c5f995d28d0396c94ad4d64891908f2a5a59
jb-mddln <13810686+jb-mddln@users.noreply.github.com>
2023-11-15 17:06:13 +0100

    refacto: renderTable refactored in PageContent and axios installed

--- react-brief-products-management/src/components/PageContent.tsx
+++ react-brief-products-management/src/components/PageContent.tsx
@@ -1,69 +1,78 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import FormAddNew from "./FormAddNew";
+import { FaEdit, FaEye, FaTrash } from "react-icons/fa";
 
 const PageContent: React.FC = () => {
-  let { type } = useParams();
+  let { type } = useParams<{ type: string }>();
 
   const renderTable = () => {
-    if (type === "products") {
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Nom</th>
-              <th>Description</th>
-              <th>Image</th>
-              <th>Stock</th>
-              <th>Prix</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Produit 1</td>
-              <td>Description du produit 1</td>
-              <td>Image du produit 1</td>
-              <td>10</td>
-              <td>19.99</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Produit 2</td>
-              <td>Description du produit 2</td>
-              <td>Image du produit 2</td>
-              <td>40</td>
-              <td>39.99</td>
-            </tr>
-          </tbody>
-        </table>
-      );
-    } else if (type === "categories") {
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Nom</th>
-              <th>Description</th>
-              <th>Image</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Catégorie 1</td>
-              <td>Description de la catégorie 1</td>
-              <td>Image de la catégorie 1</td>
-            </tr>
-          </tbody>
-        </table>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nom</th>
+            <th>Description</th>
+            <th>Image</th>
+            {type === "products" && (
+              <>
+                <th>Stock</th>
+                <th>Prix</th>
+              </>
+            )}
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {type === "products" ? (
+            <>
+              <tr>
+                <td>1</td>
+                <td>Produit 1</td>
+                <td>Description du produit 1</td>
+                <td>Image du produit 1</td>
+                <td>10</td>
+                <td>19.99</td>
+                <td>
+                  <FaEye color="blue" />
+                  <FaEdit color="green" />
+                  <FaTrash color="red" />
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Produit 2</td>
+                <td>Description du produit 2</td>
+                <td>Image du produit 2</td>
+                <td>40</td>
+                <td>39.99</td>
+                <td>
+                  <FaEye color="blue" />
+                  <FaEdit color="green" />
+                  <FaTrash color="red" />
+                </td>
+              </tr>
+            </>
+          ) : (
+            <>
+              <tr>
+                <td>1</td>
+                <td>Catégorie 1</td>
+                <td>Description de la catégorie 1</td>
+                <td>Image de la catégorie 1</td>
+                <td>
+                  <FaEye color="blue" />
+                  <FaEdit color="green" />
+                  <FaTrash color="red" />
+                </td>
+              </tr>
+            </>
+          )}
+        </tbody>
+      </table>
+    );
   };
-
   return (
     <div className="container">
       <h1>
@@ -73,6 +82,7 @@ const PageContent: React.FC = () => {
             : "Liste des catégories"
           : "Accueil"}
       </h1>
+      <div>{/* <FormAddNew type={type} /> */}</div>
       {renderTable()}
     </div>
   );
